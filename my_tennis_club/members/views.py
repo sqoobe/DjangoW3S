c044f4cdc83327f13daf38d3a6419c007bcf1918
sqoobe <sqoobe96@gmail.com>
2025-08-12 12:13:39 +0200

    Add styles to project - added static files - installed whitenoise

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -26,8 +26,10 @@ def main(request):
     return HttpResponse(template.render())
 
 def testing(request):
+  mymembers = Member.objects.all().values()
   template = loader.get_template('template.html')
   context = {
+    'mymembers': mymembers,
     'fruits': ['Apple', 'Banana', 'Cherry'],
   }
   return HttpResponse(template.render(context, request))
--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'my_tennis_club.urls'
@@ -115,7 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'productionfiles'
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'mystaticfiles'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
