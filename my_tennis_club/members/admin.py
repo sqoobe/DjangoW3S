7ac17b90329e5c8f0b0f8bb683897bfc28cd0f45
sqoobe <sqoobe96@gmail.com>
2025-08-12 12:34:59 +0200

    Finnished w3schools django tutorial

--- my_tennis_club/members/admin.py
+++ my_tennis_club/members/admin.py
@@ -6,6 +6,7 @@ from .models import Member
 
 class MemberAdmin(admin.ModelAdmin):
     list_display = ("firstname", "lastname", "joined_date",)
+    prepopulated_fields = {"slug": ("firstname", "lastname")}
 
 
 admin.site.register(Member, MemberAdmin)
--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('members/', views.members, name='members'),
-    path('members/details/<int:id>', views.details, name='details'),
+    path('members/details/<slug:slug>', views.details, name='details'),
     path('testing/', views.testing, name='testing'),
 
 
